8e66db0f2653440bf9cf5d7bcd81e808c200e892
Luis del Cid <luisdelcid@Luiss-MacBook.local>
2020-04-14 21:46:49 -0600

    IFWP

---
+++ more/media/functions.php
@@ -0,0 +1,120 @@
+<?php
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+if(!function_exists('ifwp_attachment_url_to_postid')){
+	function ifwp_attachment_url_to_postid($url = ''){
+		if($url){
+			/** original */
+			$post_id = ifwp_guid_to_postid($url);
+			if($post_id){
+				return $post_id;
+			}
+            /** resized */
+			preg_match('/^(.+)(-\d+x\d+)(\.' . substr($url, strrpos($url, '.') + 1) . ')?$/', $url, $matches);
+			if($matches){
+				$url = $matches[1];
+				if(isset($matches[3])){
+					$url .= $matches[3];
+				}
+                $post_id = ifwp_guid_to_postid($url);
+				if($post_id){
+					return $post_id;
+				}
+			}
+			/** scaled */
+			preg_match('/^(.+)(-scaled)(\.' . substr($url, strrpos($url, '.') + 1) . ')?$/', $url, $matches);
+			if($matches){
+				$url = $matches[1];
+				if(isset($matches[3])){
+					$url .= $matches[3];
+				}
+                $post_id = ifwp_guid_to_postid($url);
+				if($post_id){
+					return $post_id;
+				}
+			}
+			/** edited */
+			preg_match('/^(.+)(-e\d+)(\.' . substr($url, strrpos($url, '.') + 1) . ')?$/', $url, $matches);
+			if($matches){
+				$url = $matches[1];
+				if(isset($matches[3])){
+					$url .= $matches[3];
+				}
+                $post_id = ifwp_guid_to_postid($url);
+				if($post_id){
+					return $post_id;
+				}
+			}
+		}
+		return 0;
+	}
+}
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+if(!function_exists('ifwp_guid_to_postid')){
+	function ifwp_guid_to_postid($guid = ''){
+        global $wpdb;
+		if($guid){
+			$str = "SELECT ID FROM $wpdb->posts WHERE guid = %s";
+			$sql = $wpdb->prepare($str, $guid);
+			$post_id = $wpdb->get_var($sql);
+			if($post_id){
+				return (int) $post_id;
+			}
+		}
+		return 0;
+	}
+}
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+if(!function_exists('ifwp_maybe_require_media_functions')){
+	function ifwp_maybe_require_media_functions(){
+		if(!is_admin()){
+			require_once(ABSPATH . 'wp-admin/includes/file.php');
+			require_once(ABSPATH . 'wp-admin/includes/image.php');
+			require_once(ABSPATH . 'wp-admin/includes/media.php');
+		}
+	}
+}
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+if(!function_exists('ifwp_sideload_file')){
+    function ifwp_sideload_file($url = '', $name = '', $post = null){
+        ifwp_maybe_require_media_functions();
+    	$file_array = array(
+    		'tmp_name' => download_url($url)
+    	);
+		if(is_wp_error($file_array['tmp_name'])){
+			return $file_array['tmp_name'];
+		}
+    	$file_array['name'] = $name ? $name : basename($url);
+		$ext = pathinfo($file_array['name'], PATHINFO_EXTENSION);
+		if(!$ext and extension_loaded('fileinfo')){
+			$finfo = finfo_open(FILEINFO_MIME_TYPE);
+			$real_mime = finfo_file($finfo, $file_array['tmp_name']);
+			finfo_close($finfo);
+			foreach(wp_get_mime_types() as $exts => $mime){
+				if($mime == $real_mime){
+					$exts = explode('|', $exts);
+					$file_array['name'] .= '.' . $exts[0];
+					break;
+				}
+			}
+		}
+		$post = get_post($post);
+		$post_id = $post ? $post->ID : 0;
+		$attachment_id = media_handle_sideload($file_array, $post_id);
+		if(is_wp_error($attachment_id)){
+			@unlink($file_array['tmp_name']);
+			return $attachment_id;
+		} else {
+			return $attachment_id;
+		}
+    }
+}
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
---
+++ more/media/media.php
@@ -0,0 +1,45 @@
+<?php
+
+$tab = new _IFWP_Tab('', 'Media');
+$tab->add_switch([
+    'id' => 'add_larger_image_sizes',
+    'name' => 'Add larger image sizes?',
+    'std' => true,
+]);
+if($tab->get_option('add_larger_image_sizes', true)){
+    add_image_size('hd', 1280, 1280);
+    add_image_size('full-hd', 1920, 1920);
+    $tab->on('image_size_names_choose', function($sizes){
+        $sizes['hd'] = 'HD';
+        $sizes['full-hd'] = 'Full HD';
+		return $sizes;
+    });
+}
+$tab->add_switch([
+    'id' => 'remove_accents',
+    'name' => 'Remove accents?',
+    'std' => true,
+]);
+if($tab->get_option('remove_accents', true)){
+    $tab->on('sanitize_file_name', 'remove_accents');
+}
+$tab->add_switch([
+    'id' => 'solve_conflicts',
+    'name' => 'Solve conflicts?',
+    'std' => true,
+]);
+if($tab->get_option('solve_conflicts', true)){
+    $tab->on('wp_check_filetype_and_ext', function($wp_check_filetype_and_ext, $file, $filename, $mimes, $real_mime){
+        if($wp_check_filetype_and_ext['ext'] and $wp_check_filetype_and_ext['type']){
+    		return $wp_check_filetype_and_ext;
+    	}
+        if(0 === strpos($real_mime, 'audio/') or 0 === strpos($real_mime, 'video/')){
+            $filetype = wp_check_filetype($filename);
+            if(in_array(substr($filetype['type'], 0, strcspn($filetype['type'], '/')), array('audio', 'video'))){
+                $wp_check_filetype_and_ext['ext'] = $filetype['ext'];
+    			$wp_check_filetype_and_ext['type'] = $filetype['type'];
+            }
+        }
+    	return $wp_check_filetype_and_ext;
+    }, 10, 5);
+}
