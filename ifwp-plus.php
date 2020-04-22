fe1ef758bcbe420d29aa03e5855f992e577d1eb0
Luis del Cid <luisdelcid@Luiss-MacBook.local>
2020-04-22 13:58:23 -0600

    IFWP

--- ifwp-plus.php
+++ ifwp-plus.php
@@ -10,12 +10,12 @@ Network:
 Plugin Name: IFWP+
 Plugin URI: https://ifwp.plus
 Text Domain: ifwp-plus
-Version: 0.4.21.13
+Version: 0.4.22.1
 */
 
 defined('ABSPATH') or die("Hi there! I'm just a plugin, not much I can do when called directly.");
 
-define('IFWP_PLUS_VERSION', '0.4.21.13');
+define('IFWP_PLUS_VERSION', '0.4.22.1');
 define('IFWP_PLUS_FILE', __FILE__);
 define('IFWP_PLUS_BASENAME', plugin_basename(IFWP_PLUS_FILE));
 define('IFWP_PLUS_DIR', plugin_dir_path(IFWP_PLUS_FILE));
@@ -23,8 +23,8 @@ define('IFWP_PLUS_NAME', 'IFWP+');
 define('IFWP_PLUS_SLUG', basename(IFWP_PLUS_BASENAME, '.php'));
 define('IFWP_PLUS_URL', plugin_dir_url(IFWP_PLUS_FILE));
 
-require_once(IFWP_PLUS_DIR . 'core/_ifwp-plus.php');
-require_once(IFWP_PLUS_DIR . 'core/_ifwp-tab.php');
+require_once(IFWP_PLUS_DIR . 'core/class-ifwp-plus.php');
+require_once(IFWP_PLUS_DIR . 'core/class-ifwp-tab.php');
 require_once(IFWP_PLUS_DIR . 'core/functions.php');
 require_once(IFWP_PLUS_DIR . 'core/ifwp-plus.php');
 
