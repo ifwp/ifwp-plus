df5af96be9a2174069605d93d4695066d7e08888
Luis del Cid <luisdelcid@Luiss-MacBook.local>
2020-04-23 12:25:45 -0600

    IFWP

--- more/mail/mail.php
+++ more/mail/mail.php
@@ -11,7 +11,7 @@ $tab->add_field([
     'name' => '— Prepend:',
     'rows' => 3,
     'sanitize_callback' => 'none',
-    'std' => '<div style="width: 100%; background-color: #f1f1f1; padding: 40px 20px;"><div style="margin: 0 auto; width: 600px;"><div style="background-color: #fff; border: 1px solid #ccd0d4; color: #444; font-family: \'Helvetica Neue\', sans-serif; margin: 0; padding: 20px 40px;">',
+    'std' => '<div style="background-color: #f1f1f1; padding: 40px 20px; width: 100%;"><div style="margin: 0 auto; overflow: hidden; width: 600px;"><div style="background-color: #fff; border: 1px solid #ccd0d4;  box-shadow: 0 1px 3px rgba(0, 0, 0, .04); color: #444; font-family: \'Helvetica Neue\', sans-serif; margin: 0; padding: 20px 40px;">',
     'type' => 'textarea',
     'visible' => array('email_template', true),
 ]);
