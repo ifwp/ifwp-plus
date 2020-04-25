f6734e4705a905239fa234e11f7156d9c6cd1d2e
Luis del Cid <luisdelcid@Luiss-MacBook.local>
2020-04-25 13:08:50 -0600

    IFWP

--- more/checklist/checklist.php
+++ more/checklist/checklist.php
@@ -1,11 +1,71 @@
 <?php
 
-$tab = new _IFWP_Tab('Checklist');
-$tab->add_custom_html([
-    'name' => 'Cloudflare',
-    'std' => isset($_SERVER['HTTP_CF_CONNECTING_IP']) ? '<i class="dashicons dashicons-yes" style="color: #0073aa;"></i>' : '<i class="dashicons dashicons-no" style="color: #a00;"></i>',
-]);
-$tab->add_custom_html([
-    'name' => 'Network > IP Geolocation',
-    'std' => isset($_SERVER['HTTP_CF_IPCOUNTRY']) ? '<i class="dashicons dashicons-yes" style="color: #0073aa;"></i>' : '<i class="dashicons dashicons-no" style="color: #a00;"></i>',
-]);
+$tab = new _IFWP_Tab('Checklist', 'Cloudflare');
+
+$items = [];
+$items['Cloudflare'] = isset($_SERVER['HTTP_CF_CONNECTING_IP']) ? ifwp_dashicon_success() : ifwp_dashicon_error();
+$items['Network > IP Geolocation'] = isset($_SERVER['HTTP_CF_IPCOUNTRY']) ? ifwp_dashicon_success() : ifwp_dashicon_error();
+$post_max_size = ini_get('post_max_size');
+$post_max_size = strtoupper($post_max_size);
+$shorthand_byte_values = array(
+	'K' => KB_IN_BYTES,
+	'M' => MB_IN_BYTES,
+	'G' => GB_IN_BYTES,
+);
+foreach($shorthand_byte_values as $key => $value){
+	if(substr($post_max_size, -1) == $key){
+		$post_max_size = intval(substr($post_max_size, 0, -1)) * $value;
+		break;
+	}
+}
+$items['PHP > post_max_size <= Network > Maximum Upload Size (100 MB)'] = ($post_max_size <= (100 * MB_IN_BYTES) ? ifwp_dashicon_success() : ifwp_dashicon_error()) . ' (' . size_format($post_max_size) . ')';
+if($items){
+	$std = '<table class="wp-list-table widefat fixed striped pages">';
+	$std .= '<thead>';
+	$std .= '<tr>';
+	$std .= '<th scope="col">Item</th>';
+	$std .= '<th scope="col">Status</th>';
+	$std .= '</tr>';
+	$std .= '</thead>';
+	$std .= '<tbody>';
+	foreach($items as $item => $status){
+		$std .= '<tr>';
+		$std .= '<td>' . $item . '</td>';
+		$std .= '<td>' . $status . '</td>';
+		$std .= '</tr>';
+	}
+	$std .= '</tbody>';
+	$std .= '</table>';
+	$tab->add_custom_html([
+        'name' => 'Recommended',
+		'std' => $std,
+	]);
+}
+
+$items = [];
+$domain = wp_parse_url(site_url(), PHP_URL_HOST);
+$items['Page Rules > *' . $domain . '/*wp-login.php*'] = '— Security Level: High';
+$items['Page Rules > *' . $domain . '/*wp-admin/*'] = '— Security Level: High, Cache Level: Bypass, Disable Apps, Disable Performance';
+$items['Page Rules > *' . $domain . '/*fl_builder'] = '— Rocket Loader: Off';
+if($items){
+	$std = '<table class="wp-list-table widefat fixed striped pages">';
+	$std .= '<thead>';
+	$std .= '<tr>';
+	$std .= '<th scope="col">Item</th>';
+	$std .= '<th scope="col">Status</th>';
+	$std .= '</tr>';
+	$std .= '</thead>';
+	$std .= '<tbody>';
+	foreach($items as $item => $status){
+		$std .= '<tr>';
+		$std .= '<td>' . $item . '</td>';
+		$std .= '<td>' . $status . '</td>';
+		$std .= '</tr>';
+	}
+	$std .= '</tbody>';
+	$std .= '</table>';
+	$tab->add_custom_html([
+        'name' => 'Required',
+		'std' => $std,
+	]);
+}
